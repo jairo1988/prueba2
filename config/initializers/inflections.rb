5beef4fed4b064a7966ba49ed00e3d695353a203
Jairo <Jairo@fadrell.com>
2011-09-26 10:05:24 +0200

    Pluralizaciones españolas

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,31 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural( /$/, 'es')
+  inflect.plural(/z$/i, 'ces')
+  inflect.plural(/([aeiou])$/i, '\1s')
+
+  inflect.plural(/_(.*)$/, 'es_\1')
+  inflect.plural(/z_(.*)$/, 'ces_\1')
+  inflect.plural(/([aeiou])_(.*)$/, '\1s_\2')
+
+  inflect.singular(/s$/i, '')
+  inflect.singular(/es$/i, '')
+  inflect.singular(/ces$/i, 'z')
+  inflect.singular(/([tj])es$/i, '\1e')
+
+  inflect.singular(/(.*)s_(.*)/i, '\1_\2')
+  inflect.singular(/(.*)nes_(.*)/i, '\1n_\2')
+  inflect.singular(/(.*)les_(.*)/i, '\1l_\2')
+
+  inflect.irregular 'email', 'emails'
+  inflect.irregular 'user', 'users'
+  inflect.irregular 'session', 'sessions'
+  inflect.irregular 'password', 'passwords'
+  inflect.irregular 'slug', 'slugs'
+  inflect.irregular 'registration', 'registrations'
+  inflect.irregular 'confirmation', 'confirmations'
+
+  inflect.uncountable %w( pantalones ) # xD
+end
